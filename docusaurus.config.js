71890cd08bd0dc96a8c1b14b66b347c5dfd484af
Matt Oestreich <matthewpoestreich@gmail.com>
2020-12-20 16:49:26 -0600

    fixing minor theme issues

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,37 @@ module.exports = {
   projectName: "website", // Usually your repo name.
   themeConfig: {
     colorMode: {
+      // "light" | "dark"
       defaultMode: "light",
-      disableSwitch: true,
+
+      // Hides the switch in the navbar
+      // Useful if you want to support a single color mode
+      disableSwitch: false,
+
+      // Should we use the prefers-color-scheme media-query,
+      // using user system preferences, instead of the hardcoded defaultMode
+      respectPrefersColorScheme: false,
+
+      // Dark/light switch icon options
+      switchConfig: {
+        // Icon for the switch while in dark mode
+        darkIcon: "🌙",
+
+        // CSS to apply to dark icon,
+        // React inline style object
+        // see https://reactjs.org/docs/dom-elements.html#style
+        darkIconStyle: {
+          marginLeft: "2px",
+        },
+
+        // Unicode icons such as '\u2600' will work
+        // Unicode with 5 chars require brackets: '\u{1F602}'
+        lightIcon: "\u{1F602}",
+
+        lightIconStyle: {
+          marginLeft: "1px",
+        },
+      },
     },
     announcementBar: {
       id: "announcement_bar", // Any value that will identify this message.
@@ -18,10 +47,11 @@ module.exports = {
       textColor: "#091E42", // Defaults to `#000`.
     },
     navbar: {
-      //title: 'Home',
-      hideOnScroll: true,
+      style: "primary",
+      title: '@material-table',
+      hideOnScroll: false,
       logo: {
-        alt: "My Site Logo",
+        alt: "@material-table_logo",
         src: "img/logo.png",
       },
       links: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,26 +12,28 @@ function Home() {
 
   return (
     <Layout title={`${siteConfig.title}`}>
-      <header className={classnames("hero hero--primary", styles.heroBanner)}>
+      <header className={classnames("hero hero-primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
-            <Link style={{ margin: 5 }} className={classnames("button button--outline button--secondary button--lg", styles.getStarted)} to={useBaseUrl("docs/getting-started/install")}>
+            <Link style={{ margin: 5 }} className={classnames("button button--outline button--primary button--lg", styles.btnHover)} to={useBaseUrl("docs/getting-started/install")}>
               Docs
             </Link>
-            <Link style={{ margin: 5 }} className={classnames("button button--outline button--secondary button--lg", styles.getStarted)} to="https://github.com/material-table-core/refactor-material-table/discussions/4">
+            <Link style={{ margin: 5 }} className={classnames("button button--outline button--primary button--lg", styles.btnHover)} to="https://github.com/material-table-core/refactor-material-table/discussions/4">
               Roadmap
             </Link>
-            <Link style={{ margin: 5 }} className={classnames("button button--outline button--secondary button--lg", styles.getStarted, styles.hideMobile)} to="https://github.com/mbrn/material-table">
+            <Link style={{ margin: 5 }} className={classnames("button button--outline button--primary button--lg", styles.hideMobile, styles.btnHover)} to="https://github.com/mbrn/material-table">
               mbrn/material-table
             </Link>
           </div>
         </div>
       </header>
       <main>
-        <div className={classnames(styles.heroBanner)}>
-          <p>Fork of <code>mbrn/material-table</code></p>
+        <div className={classnames(styles.tac)}>
+          <p>
+            Fork of <code>mbrn/material-table</code>
+          </p>
         </div>
       </main>
     </Layout>

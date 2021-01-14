f6212fbca5befd9595a2e5ac89ec591c9628ac3d
Matt Oestreich <matthewpoestreich@gmail.com>
2021-01-14 10:32:47 -0600

    add basic tree data demo

--- demo.global-variables.js
+++ demo.global-variables.js
@@ -42,9 +42,85 @@ const TABLE_ICONS = {
   ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
 };
 
+const TREE_DATA = [
+  {
+    id: 1,
+    name: 'a',
+    surname: 'Baran',
+    birthYear: 1987,
+    birthCity: 63,
+    sex: 'Male',
+    type: 'adult',
+  },
+  {
+    id: 2,
+    name: 'b',
+    surname: 'Baran',
+    birthYear: 1987,
+    birthCity: 34,
+    sex: 'Female',
+    type: 'adult',
+    parentId: 1,
+  },
+  {
+    id: 3,
+    name: 'c',
+    surname: 'Baran',
+    birthYear: 1987,
+    birthCity: 34,
+    sex: 'Female',
+    type: 'child',
+    parentId: 1,
+  },
+  {
+    id: 4,
+    name: 'd',
+    surname: 'Baran',
+    birthYear: 1987,
+    birthCity: 34,
+    sex: 'Female',
+    type: 'child',
+    parentId: 3,
+  },
+  {
+    id: 5,
+    name: 'e',
+    surname: 'Baran',
+    birthYear: 1987,
+    birthCity: 34,
+    sex: 'Female',
+    type: 'child',
+  },
+  {
+    id: 6,
+    name: 'f',
+    surname: 'Baran',
+    birthYear: 1987,
+    birthCity: 34,
+    sex: 'Female',
+    type: 'child',
+    parentId: 5,
+  },
+];
+
+const TREE_COLUMNS = [
+  { title: 'Adı', field: 'name' },
+  { title: 'Soyadı', field: 'surname' },
+  { title: 'Cinsiyet', field: 'sex' },
+  { title: 'Tipi', field: 'type', removable: false },
+  { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
+  {
+    title: 'Doğum Yeri',
+    field: 'birthCity',
+    lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+  },
+]
+
 export default {
   RAND_COLOR,
   DEMO_DATA,
   DEMO_COLS,
-  TABLE_ICONS
+  TABLE_ICONS,
+  TREE_DATA,
+  TREE_COLUMNS
 }

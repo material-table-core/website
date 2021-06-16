62afe7453530fe95ac5c699eae606d073b0cc4cf
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-06-16 17:42:16 -0500

    update script for deploy

--- deploy.js
+++ deploy.js
@@ -1,37 +1,48 @@
 /**
- * 
+ *
  * In order to deploy you will need to run the following yarn
  * command, as to set the GIT_USER env var to your github username.
- * 
+ *
  * If your username were `user1234` you would run:
- * 
+ *
  *   `yarn deploy user1234`
- * 
- * 
+ *
+ *
  */
 
-const { env, argv } = process;
+const { spawn } = require("child_process");
 const { log } = console;
 
-env.GIT_USER = argv[2] ? argv[2] : null;
-if (!process.env.GIT_USER) {
-  throw new Error('Empty git user. Use like: yarn deploy oze4');
+Main();
+
+async function Main() {
+  // The first two args are supplied by default so we need 3 args, so `argv[2]`
+  if (process.argv.length <= 2) {
+    log("\nEMPTY GIT USER!!! Use command like: \n\t`yarn deploy username1234`\n");
+    process.exit(1);
+  }
+
+  process.env.GIT_USER = process.argv[2];
+  await executeAsync("docusaurus deploy" /*, [`arg1`, `arg2`] */ /*, { optionsObject } */);
 }
 
-const { exec } = require("child_process");
+async function executeAsync(exe, args, options) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(exe, args, {
+      ...options,
+      env: { ...process.env, ...options.env },
+    });
 
-log("\nPLEASE BE PATIENT THIS MAY TAKE 5 MINUTES\n");
+    child.stdout.setEncoding("utf8");
+    child.stdout.on("data", (data) => log(data));
 
-// GIT_USER env var should be set here already
-exec(`docusaurus deploy`, (error, stdout, stderr) => {
-    if (error) {
-        log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        log(`stderr: ${stderr}`);
-        return;
-    }
-    log(`stdout: ${stdout}`);
-});
+    child.stderr.setEncoding("utf8");
+    child.stderr.on("data", (data) => log(data));
 
+    child.on("error", (error) => reject(error));
+    child.on("close", (exitCode) => {
+      log("Exit code:", exitCode);
+      resolve(exitCode);
+    });
+  });
+}

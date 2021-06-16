d52887ca539eba734c652e628602f789e30bf6f1
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-06-16 17:49:45 -0500

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -13,36 +13,24 @@
 const { spawn } = require("child_process");
 const { log } = console;
 
-Main();
-
-async function Main() {
-  // The first two args are supplied by default so we need 3 args, so `argv[2]`
-  if (process.argv.length <= 2) {
-    log("\nEMPTY GIT USER!!! Use command like: \n\t`yarn deploy username1234`\n");
-    process.exit(1);
-  }
-
-  process.env.GIT_USER = process.argv[2];
-  await executeAsync("docusaurus deploy" /*, [`arg1`, `arg2`] */ /*, { optionsObject } */);
+// The first two args are supplied by default so we need 3 args, so `argv[2]`
+if (process.argv.length <= 2) {
+  log("\nEMPTY GIT USER!!! Use command like: \n\t`yarn deploy username1234`\n");
+  process.exit(1);
 }
 
-async function executeAsync(exe, args, options) {
-  return new Promise((resolve, reject) => {
-    const child = spawn(exe, args, {
-      ...options,
-      env: { ...process.env, /* ...options.env */ },
-    });
+process.env.GIT_USER = process.argv[2];
 
-    child.stdout.setEncoding("utf8");
-    child.stdout.on("data", (data) => log(data));
+const child = spawn("docusaurus deploy");
 
-    child.stderr.setEncoding("utf8");
-    child.stderr.on("data", (data) => log(data));
+child.stdout.setEncoding("utf8");
+child.stdout.on("data", (data) => log(data));
 
-    child.on("error", (error) => reject(error));
-    child.on("close", (exitCode) => {
-      log("Exit code:", exitCode);
-      resolve(exitCode);
-    });
-  });
-}
+child.stderr.setEncoding("utf8");
+child.stderr.on("data", (data) => log(data));
+
+child.on("error", (error) => reject(error));
+child.on("close", (exitCode) => {
+  log("Exit code:", exitCode);
+  resolve(exitCode);
+});
